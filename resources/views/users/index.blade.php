588915596ed72670782bd5b17d46e6bde85918b6
Thelmo Severo <thelmosevero@gmail.com>
2022-11-22 21:35:36 -0300

    SEARCH USERS

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+      <h1>Listagem de Usuários <a href="{{ route('users.create') }}" class="btn btn-success">Novo Usuário</a></h1>
+      <form action="#" method="GET">
+        <div class="row col-5 mt-1 mb-2">
+          <div class="col-auto">
+            <input type="text" name="search" id="search" class="form-control mb-2">
+          </div>
+          <div class="col-auto">
+              <button type="submit" class="btn btn-primary">Pesquisar</button>
+          </div>
+        </div>
+      </form>
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <th scope="row">{{ $user->id }}</th>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                      <a href="{{ route('users.show', ['id'=>$user->id]) }}" class="btn btn-success"><i class="bi bi-list"></i></a>
+                      <a href="{{ route('users.edit', ['id'=>$user->id]) }}" class="btn btn-primary"><i class="bi bi-pencil"></i></a>
+                    </td>
+                  </tr>
+                @endforeach
+            </tbody>
+          </table>        
+   @endsection
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+        <h1>Detalhes do Usuário</h1>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{ $user->name }}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ $user->email }}</h6>
+              <p class="card-text">User Created At: {{ $user->created_at }} / User updated at: {{$user->updated_at}}</p>
+            </div>
+            <div class="card-footer">
+              <form action="{{route('users.destroy', $user->id)}}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="btn btn-danger "><i class="bi bi-eraser"></i>APAGAR</button>
+              </form>
+            </div>
+          </div>
+@endsection
